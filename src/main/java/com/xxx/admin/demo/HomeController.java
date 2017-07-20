795016bd4d0c9eb85ff86c192e15ed8822f97fe8
xieqingqing <xieqingqing@kuaxue.com>
2017-07-20 14:41:58 +0800

    xie hello world

---
+++ src/main/java/com/xxx/admin/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.xxx.admin.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String index() {
+        return "你好，这是第一个spring boot应用11";
+    }
+}
